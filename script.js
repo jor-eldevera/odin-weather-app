225c7850b2082fbdc61990e2fe60928cfc481443
jor-eldevera <jorell.devera@gmail.com>
2023-05-10 20:10:17 -0700

    Create processJSON function to separate fetching and processing

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-getWeather("Des Moines, washington");
+getWeather("Des Moines, washington")
+    .then((response) => { processJSON(response) });
 
 async function getWeather(location) {
     let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=569bab717b714768a8f20938231105&q=${location}`, {mode: 'cors'})
     let weather = await response.json();
 
-    console.log(weather);
-    console.log(weather.current.temp_f);
-    console.log(weather.location.name);
+    return weather;    
+}
+
+function processJSON(data) {
+    console.log(data);
+    console.log(data.current.temp_f);
+    console.log(data.location.name);
 }
